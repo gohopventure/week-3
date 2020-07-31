78185557041c059301f1f2acad91b04f0e1015ce
Harold Hopson <hdhopson88@yahoo.com>
2020-07-31 14:46:04 -0700

    Initial Commit

---
+++ aspnetweb/AspNetApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+
+namespace AspNetApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = Host.CreateDefaultBuilder().ConfigureWebHostDefaults(webHost => 
+            {
+                webHost.Configure(app => 
+                {
+                    app.UseRouting();
+                    app.UseEndpoints(http => 
+                    {
+                        http.MapGet("/", async req => 
+                        {
+                            await req.Response.WriteAsync("<h1>Hello, ASPNET!<h1>");
+                        });
+                    });
+                });
+            });
+
+            server.Build().Run();
+        }
+    }
+}
