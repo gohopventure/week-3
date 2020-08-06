fbdb91eec818aeb1a94aec1a6e1579a7428c6375
Harold Hopson <hdhopson88@yahoo.com>
2020-08-05 23:01:40 -0700

    modify Order.cshtml, Program.cs

--- PizzaStoreWebApp/PizzaStore.Client/Program.cs
+++ PizzaStoreWebApp/PizzaStore.Client/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,7 +14,24 @@ namespace PizzaStore.Client
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using(var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("An error has occured while seeding");
+                    Console.WriteLine(e);
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
